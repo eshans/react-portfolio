22ccf368936874462dd84a0e48177f126549c4b0
Eric Shans <ericshans@Erics-MBP.lan>
2023-02-14 09:24:05 -0500

    updates

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import WorkImg from "../assets/projects/work.jpeg"
 
 const Work = () => {
 	return (
@@ -10,19 +11,26 @@ const Work = () => {
 					</p>
 					<p className="py-6"> // Check out some of my recent work </p>
 				</div>
+
 				{/*Grid Container*/}
 				<div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-					<div className="shadow-lg shadow-[#040c16 group container rounded-md flex justify-center items-center mx-auto content-div">
-						{/*Hover Effect*/}
+					<div
+						style={{ backgroundImage: `url(${WorkImg})` }}
+						className="shadow-lg shadow-[#040c16 group container rounded-md flex justify-center items-center mx-auto content-div">
+						{/*Hover Effect */}
 
-						<div>
-							<span></span>
-							<div>
+						<div className="opacity-0 group-hover:opacity-100">
+							<span className="text-2xl font-bold text-white tracking-wider"></span>
+							<div className="pt-8 text-center ">
 								<a href="/">
-									<button></button>
+									<button className="text-center rounded-lg px-4 m-2 bg-white text-gray-700 font-bold text-lg">
+										Demo
+									</button>
 								</a>
 								<a href="/">
-									<button></button>
+									<button className="text-center rounded-lg px-4 m-2 bg-white text-gray-700 font-bold text-lg">
+										Code
+									</button>
 								</a>
 							</div>
 						</div>
